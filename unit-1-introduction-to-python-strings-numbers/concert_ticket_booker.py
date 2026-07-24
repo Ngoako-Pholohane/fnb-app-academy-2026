8e7fa72f81741f8a0e3c6f13ffdeb2c3da51ec7e
Ngoako Pholohane <ngoakopholohane04@gmail.com>
2026-07-24 13:44:05 +0200

    Add Unit 1 Concert Ticket Booker

--- unit-1-introduction-to-python-strings-numbers/concert_ticket_booker.py
+++ unit-1-introduction-to-python-strings-numbers/concert_ticket_booker.py
@@ -1,1 +1,5 @@
+user_name = input("Enter your name: ")
+artist = input("Which band/artist would you like to see? ")
 
+
+print(f"\nHey {user_name}! Your tickets to see {artist} are booked successfully!")
